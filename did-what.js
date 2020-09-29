360a96e6d253d81459d6056352ff94202c7ec068
colleowino <colleowino@gmail.com>
2020-09-29 09:40:48 +0300

    applied all through code wars

--- did-what.js
+++ did-what.js
@@ -1,8 +1,12 @@
-window.onload = function () {
-  let titles = document.querySelectorAll(".item-title");
-  let first = titles[0];
+function buildIcon() {
   let img = document.createElement("img");
   img.setAttribute("src", chrome.runtime.getURL("verified-24px.svg"));
   img.setAttribute("class", "did-checkmark");
-  first.append(img);
+  return img;
+}
+window.onload = function () {
+  console.log("loaded");
+  let titles = document.querySelectorAll(".item-title");
+  console.log(titles.length);
+  titles.forEach((x) => x.append(buildIcon()));
 };
